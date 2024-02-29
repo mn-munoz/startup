8f006137e5450e02dc32d181972f858e381ff234
Merleth <merlethmunozra@gmail.com>
2024-02-28 17:37:18 -0700

    implemented simulation of websocket in updates.js and updated updates.html to work with it

---
+++ charadeck/updates.js
@@ -0,0 +1,4 @@
+setInterval(() => {
+    const updateText = document.querySelector('.updates');
+    updateText.innerHTML = '<p><span>Mei </span>included a new card on his deck</p>' + updateText.innerHTML ;
+}, 6000);
