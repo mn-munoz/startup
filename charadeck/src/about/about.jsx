e38fd8185517ba7fcef684d5ee8b8a3d6e54908a
Merleth <merlethmunozra@gmail.com>
2024-04-10 12:11:43 -0600

    chance about.css

--- charadeck/src/about/about.jsx
+++ charadeck/src/about/about.jsx
@@ -40,7 +40,7 @@ export function About(props) {
     img = <img src={imageUrl} alt='stock background'/>;
   }
   return (
-    <main>
+    <main  className='about'>
       <div className="heading-image">
           {img}
       </div>
